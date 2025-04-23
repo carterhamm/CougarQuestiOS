922c3817ca76ced21add4bee14b3bf1412c05325
carterhamm <75005431+carterhamm@users.noreply.github.com>
2025-04-23 08:00:34 -0600

    Initial Commit

---
+++ CougarQuest/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CougarQuest
+//
+//  Created by Carter Hammond on 4/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CougarQuestTests/CougarQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CougarQuestTests.swift
+//  CougarQuestTests
+//
+//  Created by Carter Hammond on 4/23/25.
+//
+
+import Testing
+@testable import CougarQuest
+
+struct CougarQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
